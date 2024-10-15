98185e13ef7ef9eda8b89efeced53e5a4bd82b71
serk03 <onalserkan@googlemail.com>
2024-10-15 13:59:38 +0100

    completed task3

--- nc_news_app/app.js
+++ nc_news_app/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("../nc_news_app/controller");
+const endpoints = require("../endpoints.json");
 
 app.get("/api/topics", getTopics);
+
+app.get("/api", (request, response) => {
+  response.status(200).send({ endpoints });
+});
 
 module.exports = app;
